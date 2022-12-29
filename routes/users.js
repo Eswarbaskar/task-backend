c32340e9abb9357f33a7f33907d1225a2e9122db
Eswarbaskar <eswarbilla89@gmail.com>
2022-12-29 19:50:51 +0530

    commit

--- routes/users.js
+++ routes/users.js
@@ -1,139 +1,139 @@
 var express = require('express');
 var router = express.Router();
-var {dbName,dbUrl,mongodbClient,mongodb}=require('../dbconfig')
+var { dbName, dbUrl, mongodbClient, mongodb } = require('../dbconfig')
 const client = new mongodbClient(dbUrl);
-var {hashCompare,hashpassword}=require('../bin/auth');
+var { hashCompare, hashpassword } = require('../bin/auth');
 
 
-router.get('/', async(req,res)=>{
+router.get('/', async (req, res) => {
   await client.connect();
   try {
     let db = await client.db(dbName);
     let user = await db.collection('users').find().toArray()
     res.send({
-      statusCode:200,
-      data:user
+      statusCode: 200,
+      data: user
     })
-    
+
   } catch (error) {
     console.log(error);
     res.send({
-      statusCode:400,
-      message:"bad requst"
+      statusCode: 400,
+      message: "bad requst"
     })
   }
-  finally{
+  finally {
     client.close()
   }
 })
 
 /* sign up */
-router.post('/sign-up', async(req, res)=>{
+router.post('/sign-up', async (req, res) => {
   await client.connect();
   try {
-    const db =await client.db(dbName);
-    const user = await db.collection('users').find({email:req.body.email}).toArray()
-    if(user.length===0){
-      if(user.password===user.confirmPassword){
-        let user={...req.body,age:'',dob:'',mobile:'',gender:''}
+    const db = await client.db(dbName);
+    const user = await db.collection('users').find({ email: req.body.email }).toArray()
+    if (user.length === 0) {
+      if (user.password === user.confirmPassword) {
+        let user = { ...req.body, age: '', dob: '', mobile: '', gender: '' }
         req.body.password = await hashpassword(req.body.password);
         let users = await db.collection('users').insertOne(user);
 
         res.send({
-          statusCode:200,
-          message:"User Added Successfully"
+          statusCode: 200,
+          message: "User Added Successfully"
         })
       }
-      else{
+      else {
         res.send({
-          statusCode:400,
-          message:"confirmpassword not match"
+          statusCode: 400,
+          message: "confirmpassword not match"
         })
       }
     }
-    else{
+    else {
       res.send({
-        statusCode:200,
-        message:"User already exists,kindly login"
+        statusCode: 200,
+        message: "User already exists,kindly login"
       })
     }
-    
+
   } catch (error) {
     console.log(error)
     res.send({
-      statusCode:500,
-      message:"Internal Server Error"
+      statusCode: 500,
+      message: "Internal Server Error"
     })
   }
-  finally{
+  finally {
     client.close()
   }
 });
 
 // login
 
-router.post('/login', async(req, res)=>{
+router.post('/login', async (req, res) => {
   await client.connect();
   try {
-    const db =await client.db(dbName);
-    const user =await db.collection('users').find({email:req.body.email}).toArray()
-    if(user.length===1){
-      let hashResult=await hashCompare(req.body.password,user[0].password)
-      if(hashResult){
+    const db = await client.db(dbName);
+    const user = await db.collection('users').find({ email: req.body.email }).toArray()
+    if (user.length === 1) {
+      let hashResult = await hashCompare(req.body.password, user[0].password)
+      if (hashResult) {
         res.send({
-          statusCode:200,
-          message:"User Logged in Successfully"
+          statusCode: 200,
+          message: "User Logged in Successfully"
         })
       }
-      else{
+      else {
         res.send({
-          statusCode:401,
-          message:"correct your password"
+          statusCode: 401,
+          message: "correct your password"
         })
       }
     }
-      else{
-        res.send({
-          statusCode:401,
-          message:"User Does Not Exist"
-        })
-      }
-    
-    
-    
+    else {
+      res.send({
+        statusCode: 401,
+        message: "User Does Not Exist"
+      })
+    }
+
+
+
   } catch (error) {
     console.log(error)
     res.send({
-      statusCode:500,
-      message:"Internal Server Error"
+      statusCode: 500,
+      message: "Internal Server Error"
     })
   }
-  finally{
+  finally {
     client.close()
   }
 });
 
-router.put('/update/:id',async(req,res)=>{
-  await client.connect()
+router.put('/update/:id', async(req, res) => {
+  await client.connect();
   try {
     let db = await client.db(dbName);
-    let user = await db.collection('users').updateOne({_id:mongodb.ObjectId(req.params.id)},{$set:req.body}).toArray()
+    let user = await db.collection('users').findOneAndUpdate({ _id: req.params.id }, { $set:req.body  })
     res.send({
-           statusCode:200,
-          message:"Successfully Added",
-          user
+      statusCode: 200,
+      message: "Successfully Added",
+      user
     })
-    
+
   } catch (error) {
     console.log(error)
     res.send({
-      statusCode:500,
-      message:"Internal Server Error"
+      statusCode: 500,
+      message: "Internal Server Error"
     })
-    
+
   }
-  finally{
-    await client.close()
+  finally {
+     client.close()
   }
 })
 
